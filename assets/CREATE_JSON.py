715c8a70ef40d7751885ccd90a672376f42d98ae
Antoine Carlier <antoinehuguescarlier44@gmail.com>
2022-02-11 00:17:38 +0100

    push test github pages

---
+++ assets/CREATE_JSON.py
@@ -0,0 +1,17 @@
+from os import listdir
+from os.path import isfile, join
+import json
+folders = ["tile", "objects", "layer", "frontLayer", "background"]
+
+for a in folders:
+    onlyfiles = [f for f in listdir(a) if isfile(join(a, f))]
+    to_save = {}
+    i = 0
+    for b in onlyfiles:
+        print (b[-4:])
+        if b[-4:] == ".png":
+            to_save[str(i)] = b
+            i += 1
+    with open(a + '/data.json', 'w') as outfile:
+        json.dump(to_save, outfile)
+
